3aa33e5c0d7a3e594efea07645d0263fb229b06c
fupeng <fup1990@163.com>
2018-01-13 20:45:55 +0800

    优化

--- poems_main.py
+++ poems_main.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import numpy as np
-from poems_train import process_poems, rnn_model
+from poems_train import load_poems, process_poems, rnn_model
 
 start_token = 'B'
 end_token = 'E'
 model_dir = 'model/'
-corpus_file = 'poems.txt'
+corpus_file = "poems.txt"
 
 def to_word(predict, vocabs):
     t = np.cumsum(predict)
@@ -18,9 +18,10 @@ def to_word(predict, vocabs):
 
 def gen_poem():
     batch_size = 1
-    words, dict_words, vector = process_poems(corpus_file)
+    poems = load_poems(corpus_file)
+    words, dict_words, vector = process_poems(poems)
 
-    input_data = tf.Variable(tf.int32, [batch_size, None])
+    input_data = tf.placeholder(tf.int32, [batch_size, None])
 
     end_points = rnn_model(input_data, None, len(vector))
 
@@ -33,7 +34,7 @@ def gen_poem():
 
         x = np.array([list(map(dict_words.get, start_token))])
         [predict, last_state] = sess.run([end_points['prediction'], end_points['last_state']], feed_dict={input_data: x})
-        word = to_word(predict, vector)
+        word = to_word(predict, words)
         poem = ''
         i = 0
         while word != end_token:
@@ -44,7 +45,7 @@ def gen_poem():
             x = np.zeros((1, 1))
             x[0, 0] = dict_words[word]
             [predict, last_state] = sess.run([end_points['prediction'], end_points['last_state']], feed_dict={input_data: x, end_points['initial_state']: last_state})
-            word = to_word(predict, vector)
+            word = to_word(predict, words)
         return poem
 
 def pretty_print_poem(poem):
